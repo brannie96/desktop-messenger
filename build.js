a6520d76d0d9d308a2b27d0165a7e4d6370d325c
Vladislav Stoitsov <vlado@netlabs.bg>
2015-04-22 18:50:47 +0300

    Added taskbar badge (OSX and Windows) - showing the count of new messages in the icon

--- build.js
+++ build.js
@@ -1,7 +1,10 @@
+var semver = require('semver');
+var request = require("request");
 var Client = require('ftp');
 var NwBuilder = require('node-webkit-builder');
 var file_system = require('fs');
 var archiver = require('archiver');
+var settings = require("./settings.json");
 
 function build() {
     this.package = {};
@@ -14,7 +17,7 @@ build.prototype.update_build = function () {
     var json_file = "./source/package.json";
     json = file_system.readFileSync(json_file);
     json = JSON.parse(json);
-    json.build = Number(json.build) + 1;
+    json.version = semver.inc(json.version, "patch");
     json.build_date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
     new_json = JSON.stringify(json, null, "\t");
     file_system.writeFileSync(json_file, new_json);
@@ -29,9 +32,9 @@ build.prototype.connect_ftp = function () {
     $this.ftp = new Client();
     // connect to localhost:21 as anonymous
     $this.ftp.connect({
-        host: "dev.netlabs.bg",
-        user: "messenger@dev.netlabs.bg", // defaults to "anonymous"
-        password: "(CKO6LRzVX;i" // defaults to "@anonymous"
+        host: settings.ftp_hostname,
+        user: settings.ftp_username,
+        password: settings.ftp_password
     });
 };
 build.prototype.log = function (text) {
@@ -41,57 +44,69 @@ build.prototype.log = function (text) {
 build.prototype.build_packages = function () {
     var $this = this;
     this.update_build();
-    $this.log('Build of version ' + $this.package.version + ' (build: ' + $this.package.build + ') started ...');
-    var pjson = require('./source/package.json');
-    var nw = new NwBuilder({
-        files: 'source/**', // use the glob format
-        appName: 'Messenger',
-        //    buildType: function () {return $this.appVersion;},
-        winIco: 'messenger.ico',
-        platforms: ['win32', 'win64']
-        //        platforms: ['win32', 'win64', 'osx32', 'osx64', 'linux32', 'linux64']
-    });
-    nw.build().then(function () {
-        $this.log('Build completed! Start compressing files ...');
-        $this.current_file = 1;
-        nw.options.platforms.forEach(function (entry) {
-            build = "./build/" + entry;
-            dir = './build/packaged/';
-            if (!file_system.existsSync(dir)) {
-                file_system.mkdirSync(dir);
-            }
-            var output = file_system.createWriteStream('./build/packaged/Messenger-' + entry + '.zip');
-            var archive = archiver('zip');
-            output.on('close', function () {
-                $this.log('created ' + entry + ': size: ' + archive.pointer() + ' total bytes');
-                $this.ftp.put('./build/packaged/Messenger-' + entry + '.zip', '/latest/Messenger-' + entry + '.zip', function (hadError) {
-                    $this.log("Uploaded Messenger-" + entry + ".zip; File " + $this.current_file + "/" + nw.options.platforms.length);
-                    if ($this.current_file == nw.options.platforms.length) {
-                        $this.completed();
-                    } else {
-                        $this.current_file++;
+    $this.log('Build of version ' + $this.package.version + ' (build: ' + $this.package.build_date + ') started ...');
+
+    // retrieve last pushed package.json
+    // if version is the same it will not publish the build (dev version)
+    request({
+        url: 'http://messenger.dev.netlabs.bg/latest/package.json',
+        json: true
+    }, function (error, response, latest_json) {
+        if (!error && response.statusCode === 200) {
+            var nw = new NwBuilder({
+                files: 'source/**', // use the glob format
+                appName: 'Messenger',
+                //    buildType: function () {return $this.appVersion;},
+                winIco: 'messenger.ico',
+        //        platforms: ['win32', 'win64']
+		        platforms: ['win32', 'win64', 'osx32', 'osx64', 'linux32', 'linux64']
+            });
+            nw.build().then(function () {
+                $this.log('Build completed! Start compressing files ...');
+                $this.current_file = 1;
+                nw.options.platforms.forEach(function (entry) {
+                    build = "./build/" + entry;
+                    dir = './build/packaged/';
+                    if (!file_system.existsSync(dir)) {
+                        file_system.mkdirSync(dir);
                     }
-                    if (hadError) {
-                        throw hadError;
-                    }
+                    var output = file_system.createWriteStream('./build/packaged/Messenger-' + entry + '.zip');
+                    var archive = archiver('zip');
+                    output.on('close', function () {
+                        $this.log('created ' + entry + ': size: ' + archive.pointer() + ' total bytes');
+                        $this.ftp.put('./build/packaged/Messenger-' + entry + '.zip', '/latest/Messenger-' + entry + '.zip', function (hadError) {
+                            $this.log("Uploaded Messenger-" + entry + ".zip; File " + $this.current_file + "/" + nw.options.platforms.length);
+                            if ($this.current_file == nw.options.platforms.length) {
+                                $this.completed();
+                            } else {
+                                $this.current_file++;
+                            }
+                            if (hadError) {
+                                throw hadError;
+                            }
+                        });
+                    });
+                    archive.on('error', function (err) {
+                        throw err;
+                    });
+                    archive.pipe(output);
+                    archive.bulk([{
+                        expand: true,
+                        cwd: './build/Messenger/' + entry + '/',
+                        src: ['**'],
+                        dest: ''
+                    }]);
+                    archive.finalize();
                 });
             });
-            archive.on('error', function (err) {
-                throw err;
-            });
-            archive.pipe(output);
-            archive.bulk([{
-                expand: true,
-                cwd: './build/Messenger/' + entry + '/',
-                src: ['**'],
-                dest: ''
-            }]);
-            archive.finalize();
-        });
+        }
     });
 };
 build.prototype.completed = function () {
-    this.log("Build completed");
-    this.ftp.end();
+    $this = this;
+    $this.ftp.put('./source/package.json', '/latest/package.json', function (hadError) {
+        $this.log("Build completed");
+        $this.ftp.end();
+    });
 };
 new build();
